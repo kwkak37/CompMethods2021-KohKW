f5db568768afdf51f81147e2ede728ed285b0d8b
Kyung Woong Koh <kwkak37@gmail.com>
2021-10-19 13:26:54 -0500

    ProjectKWK compiles!

--- reproduce-BufferStockTheory.sh
+++ reproduce-BufferStockTheory.sh
@@ -7,7 +7,7 @@ scriptDir="$(dirname "$0")"
 
 echo '' ; echo 'Reproduce text of paper:' ; echo ''
 
-texname=ProjectKWK
+texname=BufferStockTheory
 output_directory='LaTeX'
 
 # Make figures that get made by executing a latex file
